84a5df38e1b909a73c5cdea07f096dcdcfcbf655
Anstice2018 <35337481+Anstice2018@users.noreply.github.com>
2021-01-20 17:17:24 +0800

    Update example.vue

--- example.vue
+++ example.vue
@@ -20,3 +20,45 @@
 <h3> hello </h3>
 </div>
 </template>
+
+
+
+<script>
+export default{
+    data() {
+        return {
+            name1: 'value1',
+            name2: 'value2',
+            myBoolean: true,
+            myNumber: 1
+        }
+    },
+    methods: {
+        toggleMethod() {
+            this.myboolean = !this.myboolean
+        },
+        aPlusMethod(a, b){
+            return{
+                a+b;
+            }
+        }
+    },
+    computed: {
+        variable1(a, b) {
+            return {
+                a+b;
+            }
+        }
+    },
+    watch: {
+        myBoolean(){
+            alert('myBoolean has been changed!');
+        },
+        aPlusMethod(){
+            alert('aPlusMethod has been triggered!');
+        }
+    }
+}
+</script>
+
+
