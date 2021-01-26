eab7c43f04f7b9fc5fcff9601f14a379f97c5975
Anstice2018 <35337481+Anstice2018@users.noreply.github.com>
2021-01-26 17:20:49 +0800

    Create InfoCard.vue

---
+++ InfoCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>{{ name }}</h2>
+        <h6>Phone: {{ phoneNumber }}</h6>
+        <h6>email: {{ emailAddress }}</h6>
+        <h6>favarite: {{ isFavorite }}</h6>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'name',
+        'phoneNumber',
+        'emailAddress',
+        'isFavorite'
+    ]
+}
+</script>
